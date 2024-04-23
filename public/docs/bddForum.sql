3a2d03537e8a53b5396620f3fdd8060ba7486794
Flozerty <floris.louerat@gmail.com>
2024-04-23 17:23:26 +0200

    update db for framework compatibility

--- public/docs/bddForum.sql
+++ public/docs/bddForum.sql
@@ -24,14 +24,14 @@ USE `forum`;
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE IF NOT EXISTS `category` (
   `id_category` int NOT NULL AUTO_INCREMENT,
-  `category_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
   `icone` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_category`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Listage des données de la table forum.category : ~6 rows (environ)
 DELETE FROM `category`;
-INSERT INTO `category` (`id_category`, `category_name`, `icone`) VALUES
+INSERT INTO `category` (`id_category`, `name`, `icone`) VALUES
 	(1, 'jeux de société', NULL),
 	(2, 'jeux vidéos', NULL),
 	(3, 'cuisine', NULL),
@@ -39,24 +39,24 @@ INSERT INTO `category` (`id_category`, `category_name`, `icone`) VALUES
 	(5, 'musique', NULL),
 	(6, 'cinema', NULL);
 
--- Listage de la structure de table forum. message
-DROP TABLE IF EXISTS `message`;
-CREATE TABLE IF NOT EXISTS `message` (
-  `id_message` int NOT NULL AUTO_INCREMENT,
-  `date_message` datetime DEFAULT NULL,
-  `text_message` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `id_topic` int DEFAULT NULL,
-  `id_user` int DEFAULT NULL,
-  PRIMARY KEY (`id_message`),
-  KEY `FK_message_sujet` (`id_topic`) USING BTREE,
-  KEY `FK_message_utilisateur` (`id_user`) USING BTREE,
-  CONSTRAINT `FK_message_sujet` FOREIGN KEY (`id_topic`) REFERENCES `topic` (`id_sujet`),
-  CONSTRAINT `FK_message_utilisateur` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`)
+-- Listage de la structure de table forum. post
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE IF NOT EXISTS `post` (
+  `id_post` int NOT NULL AUTO_INCREMENT,
+  `dateMessage` datetime DEFAULT NULL,
+  `messageContent` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `topic_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id_post`) USING BTREE,
+  KEY `FK_message_sujet` (`topic_id`) USING BTREE,
+  KEY `FK_message_utilisateur` (`user_id`) USING BTREE,
+  CONSTRAINT `FK_message_sujet` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`id_topic`),
+  CONSTRAINT `FK_message_utilisateur` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Listage des données de la table forum.message : ~5 rows (environ)
-DELETE FROM `message`;
-INSERT INTO `message` (`id_message`, `date_message`, `text_message`, `id_topic`, `id_user`) VALUES
+-- Listage des données de la table forum.post : ~5 rows (environ)
+DELETE FROM `post`;
+INSERT INTO `post` (`id_post`, `dateMessage`, `messageContent`, `topic_id`, `user_id`) VALUES
 	(1, '2024-04-17 12:21:11', 'Bonjour, j\'essaye de faire une omelette à l\'oignon mais j\'arrive pas a casser mes oeufs, quelqu\'un peut m\'aider?', 1, 2),
 	(2, '2024-04-17 12:25:21', 'Tu prends tes oeufs et tu les jettes dans la poele, apres ils sont cassés', 1, 3),
 	(3, '2024-04-17 12:40:40', 'J\'ai essayé mais ça marche pas très bien ils sont explosés maintenant...', 1, 2),
@@ -66,24 +66,25 @@ INSERT INTO `message` (`id_message`, `date_message`, `text_message`, `id_topic`,
 -- Listage de la structure de table forum. topic
 DROP TABLE IF EXISTS `topic`;
 CREATE TABLE IF NOT EXISTS `topic` (
-  `id_sujet` int NOT NULL AUTO_INCREMENT,
-  `date_creation` date DEFAULT NULL,
+  `id_topic` int NOT NULL AUTO_INCREMENT,
+  `dateCreation` date DEFAULT NULL,
   `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `id_category` int DEFAULT NULL,
-  `id_user` int DEFAULT NULL,
-  PRIMARY KEY (`id_sujet`),
-  KEY `FK_sujet_categorie` (`id_category`) USING BTREE,
-  KEY `FK_sujet_utilisateur` (`id_user`) USING BTREE,
-  CONSTRAINT `FK_sujet_categorie` FOREIGN KEY (`id_category`) REFERENCES `category` (`id_category`),
-  CONSTRAINT `FK_sujet_utilisateur` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`)
+  `closed` tinyint DEFAULT NULL,
+  `category_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id_topic`) USING BTREE,
+  KEY `FK_sujet_categorie` (`category_id`) USING BTREE,
+  KEY `FK_sujet_utilisateur` (`user_id`) USING BTREE,
+  CONSTRAINT `FK_sujet_categorie` FOREIGN KEY (`category_id`) REFERENCES `category` (`id_category`),
+  CONSTRAINT `FK_sujet_utilisateur` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Listage des données de la table forum.topic : ~3 rows (environ)
+-- Listage des données de la table forum.topic : ~0 rows (environ)
 DELETE FROM `topic`;
-INSERT INTO `topic` (`id_sujet`, `date_creation`, `title`, `id_category`, `id_user`) VALUES
-	(1, '2024-04-17', 'omelette à l\'oignon', 3, 2),
-	(2, '2024-04-18', 'monter la forge sur skyrim', 2, 2),
-	(3, '2024-04-19', 'qui qui joue au jeu de go', 1, 1);
+INSERT INTO `topic` (`id_topic`, `dateCreation`, `title`, `closed`, `category_id`, `user_id`) VALUES
+	(1, '2024-04-17', 'omelette à l\'oignon', NULL, 3, 2),
+	(2, '2024-04-18', 'monter la forge sur skyrim', NULL, 2, 2),
+	(3, '2024-04-19', 'qui qui joue au jeu de go', NULL, 1, 1);
 
 -- Listage de la structure de table forum. user
 DROP TABLE IF EXISTS `user`;
@@ -92,7 +93,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `pseudo` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `date_inscription` date NOT NULL,
+  `dateInscription` date NOT NULL,
   `role` varchar(50) DEFAULT NULL,
   `avatar` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_user`) USING BTREE
@@ -100,7 +101,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 -- Listage des données de la table forum.user : ~4 rows (environ)
 DELETE FROM `user`;
-INSERT INTO `user` (`id_user`, `pseudo`, `email`, `password`, `date_inscription`, `role`, `avatar`) VALUES
+INSERT INTO `user` (`id_user`, `pseudo`, `email`, `password`, `dateInscription`, `role`, `avatar`) VALUES
 	(1, 'flozerty', 'floris.louerat@gmail.com', '12345azerty', '2024-04-19', 'admin', NULL),
 	(2, 'bob', 'bob.bob@bob.bob', 'bobobobobo', '2024-04-19', NULL, NULL),
 	(3, 'test', 'test@gmail.com', 'test123', '2024-04-19', NULL, NULL),
