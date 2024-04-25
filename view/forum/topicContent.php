8f6078d625ca83255e89f9c2579f28fad6328865
flozerty <floris.louerat@gmail.com>
2024-04-25 16:21:36 +0200

    posts structuration

--- view/forum/topicContent.php
+++ view/forum/topicContent.php
@@ -6,21 +6,33 @@
 <h1><?= $topic ?></h1>
 <p><?= $topic->getIntro() ?></p>
 
+<section id="posts-container">
 
-
-<!-- S'il y a déjà des messages dans le topic -->
-<?php if(!empty($posts)) {
+  <!-- S'il y a déjà des messages dans le topic -->
+  <?php if(!empty($posts)) {
   foreach($posts as $post) { ?>
-<p>
-  <?= $post ?> par <?= $post->getUser() ?>
-</p>
-<?php }
+
+  <div class="topic-post">
+    <div class="postInfos">
+      <p><?= $post->getUser() ?></p>
+      <p>il y a <?= $post->getPostDate() ?></p>
+    </div>
+    <div class="content-bubble">
+      <p>
+        <?= $post ?>
+      </p>
+    </div>
+  </div>
+
+  <?php }
 } else { ?>
-<!-- S'il n'y a aucun message -->
-<p>Aucun message</p>
-<?php } ?>
+  <!-- S'il n'y a aucun message -->
+  <p>Aucun message</p>
+  <?php } ?>
+
+</section>
 
 <form action="#" method="post" id="newPostForm">
-  <textarea id="newPost" name="newPost" placeholder="ajoutez un commentaire"></textarea>
+  <textarea id="newPost" name="newPost" cols="50" rows="10" placeholder="ajoutez un commentaire"></textarea>
   <input type="submit" value="envoyer" class="submitButton">
 </form>
