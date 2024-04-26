32780963b17dcd15ed0d066afd73770e596c2598
Flozerty <floris.louerat@gmail.com>
2024-04-26 08:50:59 +0200

    updatee db

--- public/docs/bdd.sql
+++ public/docs/bdd.sql
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `category` (
   PRIMARY KEY (`id_category`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Listage des données de la table forum.category : ~6 rows (environ)
+-- Listage des données de la table forum.category : ~7 rows (environ)
 DELETE FROM `category`;
 INSERT INTO `category` (`id_category`, `name`, `icone`) VALUES
 	(1, 'jeux de société', '<i class="fa-solid fa-dice"></i>'),
@@ -44,9 +44,9 @@ INSERT INTO `category` (`id_category`, `name`, `icone`) VALUES
 DROP TABLE IF EXISTS `post`;
 CREATE TABLE IF NOT EXISTS `post` (
   `id_post` int NOT NULL AUTO_INCREMENT,
-  `dateMessage` datetime DEFAULT NULL,
+  `postDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `messageContent` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `deleted` tinyint(1) DEFAULT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
   `topic_id` int DEFAULT NULL,
   `user_id` int DEFAULT NULL,
   PRIMARY KEY (`id_post`) USING BTREE,
@@ -58,21 +58,21 @@ CREATE TABLE IF NOT EXISTS `post` (
 
 -- Listage des données de la table forum.post : ~5 rows (environ)
 DELETE FROM `post`;
-INSERT INTO `post` (`id_post`, `dateMessage`, `messageContent`, `deleted`, `topic_id`, `user_id`) VALUES
-	(1, '2024-04-17 12:21:11', 'Bonjour, j\'essaye de faire une omelette à l\'oignon mais j\'arrive pas a casser mes oeufs, quelqu\'un peut m\'aider?', NULL, 1, 2),
-	(2, '2024-04-17 12:25:21', 'Tu prends tes oeufs et tu les jettes dans la poele, apres ils sont cassés', NULL, 1, 3),
-	(3, '2024-04-17 12:40:40', 'J\'ai essayé mais ça marche pas très bien ils sont explosés maintenant...', NULL, 1, 2),
-	(4, '2024-04-17 13:00:00', 'Au secours ma poele est en feu à l\'aide', NULL, 1, 2),
-	(5, '2024-04-19 11:31:17', 'help', NULL, 1, 2);
+INSERT INTO `post` (`id_post`, `postDate`, `messageContent`, `deleted`, `topic_id`, `user_id`) VALUES
+	(1, '2024-04-17 12:21:11', 'Bonjour, j\'essaye de faire une omelette à l\'oignon mais j\'arrive pas a casser mes oeufs, quelqu\'un peut m\'aider?', 0, 1, 2),
+	(2, '2024-04-17 12:25:21', 'Tu prends tes oeufs et tu les jettes dans la poele, apres ils sont cassés', 0, 1, 3),
+	(3, '2024-04-17 12:40:40', 'J\'ai essayé mais ça marche pas très bien ils sont explosés maintenant...', 0, 1, 2),
+	(4, '2024-04-17 13:00:00', 'Au secours ma poele est en feu à l\'aide', 0, 1, 2),
+	(5, '2024-04-19 11:31:17', 'help', 0, 1, 2);
 
 -- Listage de la structure de table forum. topic
 DROP TABLE IF EXISTS `topic`;
 CREATE TABLE IF NOT EXISTS `topic` (
   `id_topic` int NOT NULL AUTO_INCREMENT,
   `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `dateCreation` date DEFAULT NULL,
-  `intro` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `closed` tinyint(1) DEFAULT NULL,
+  `creationDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `intro` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `closed` tinyint(1) NOT NULL DEFAULT '0',
   `category_id` int DEFAULT NULL,
   `user_id` int DEFAULT NULL,
   PRIMARY KEY (`id_topic`) USING BTREE,
@@ -84,10 +84,10 @@ CREATE TABLE IF NOT EXISTS `topic` (
 
 -- Listage des données de la table forum.topic : ~3 rows (environ)
 DELETE FROM `topic`;
-INSERT INTO `topic` (`id_topic`, `title`, `dateCreation`, `intro`, `closed`, `category_id`, `user_id`) VALUES
-	(1, 'omelette à l\'oignon', '2024-04-17', 'j\'ai besoin d\'aide', NULL, 3, 2),
-	(2, 'monter la forge sur skyrim', '2024-04-18', 'vous faites comment?', NULL, 2, 2),
-	(3, 'qui qui joue au jeu de go', '2024-04-19', 'Je recherche des joueurs sur Strasbourg', NULL, 1, 1);
+INSERT INTO `topic` (`id_topic`, `title`, `creationDate`, `intro`, `closed`, `category_id`, `user_id`) VALUES
+	(1, 'omelette à l\'oignon', '2024-04-17 00:00:00', 'j\'ai besoin d\'aide', 0, 3, 2),
+	(2, 'monter la forge sur skyrim', '2024-04-18 00:00:00', 'vous faites comment?', 0, 2, 2),
+	(3, 'qui qui joue au jeu de go', '2024-04-19 00:00:00', 'Je recherche des joueurs sur Strasbourg', 0, 1, 1);
 
 -- Listage de la structure de table forum. user
 DROP TABLE IF EXISTS `user`;
@@ -95,8 +95,8 @@ CREATE TABLE IF NOT EXISTS `user` (
   `id_user` int NOT NULL AUTO_INCREMENT,
   `pseudo` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `dateInscription` date NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `dateInscription` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `role` varchar(50) DEFAULT NULL,
   `avatar` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_user`) USING BTREE
@@ -105,10 +105,10 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Listage des données de la table forum.user : ~4 rows (environ)
 DELETE FROM `user`;
 INSERT INTO `user` (`id_user`, `pseudo`, `email`, `password`, `dateInscription`, `role`, `avatar`) VALUES
-	(1, 'flozerty', 'floris.louerat@gmail.com', '12345azerty', '2024-04-19', 'admin', NULL),
-	(2, 'bob', 'bob.bob@bob.bob', 'bobobobobo', '2024-04-19', NULL, NULL),
-	(3, 'test', 'test@gmail.com', 'test123', '2024-04-19', NULL, NULL),
-	(6, 'arklane', 'arklane.gmail.com', 'arklemochedu73', '2024-04-19', 'modo', NULL);
+	(1, 'flozerty', 'floris.louerat@gmail.com', '12345azerty', '2024-04-19 00:00:00', 'admin', NULL),
+	(2, 'bob', 'bob.bob@bob.bob', 'bobobobobo', '2024-04-19 00:00:00', NULL, NULL),
+	(3, 'test', 'test@gmail.com', 'test123', '2024-04-19 00:00:00', NULL, NULL),
+	(6, 'arklane', 'arklane.gmail.com', 'arklemochedu73', '2024-04-19 00:00:00', 'modo', NULL);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
