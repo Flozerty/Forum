95f047c1992945c4fc80bc6194b1ce7644fe10bf
flozerty <floris.louerat@gmail.com>
2024-05-06 12:00:18 +0200

    display user nav list

--- public/js/userPanel.js
+++ public/js/userPanel.js
@@ -2,3 +2,15 @@ const navRight = document.querySelector('#nav-right');
 const headerNavList = navRight.querySelector('#headerNavList');
 const caret = navRight.querySelector('#userPanelShow');
 
+show = false;
+
+caret.addEventListener('click', () => {
+  if (show) {
+    caret.classList.remove("rotate180");
+    headerNavList.style.top = "-300%";
+  } else {
+    caret.classList.add("rotate180");
+    headerNavList.style.top = "150%";
+  }
+  show = !show;
+})
