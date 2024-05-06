66ed7c0352d24f5c345a1235a41c3cfea622033f
flozerty <floris.louerat@gmail.com>
2024-05-06 14:22:16 +0200

    remove user navTools when click on main

--- public/js/userPanel.js
+++ public/js/userPanel.js
@@ -1,16 +1,32 @@
+const main = document.querySelector("main");
 const navRight = document.querySelector('#nav-right');
+const userContainer = navRight.querySelector("#userContainer");
 const headerNavList = navRight.querySelector('#headerNavList');
 const caret = navRight.querySelector('#userPanelShow');
 
+
 show = false;
 
-caret.addEventListener('click', () => {
+userContainer.addEventListener('click', () => {
   if (show) {
     caret.classList.remove("rotate180");
-    headerNavList.style.top = "-300%";
+    headerNavList.style.top = "-400%";
   } else {
     caret.classList.add("rotate180");
     headerNavList.style.top = "150%";
   }
   show = !show;
 })
+
+main.addEventListener("click", () => {
+  if (show) {
+    caret.classList.remove("rotate180");
+    headerNavList.style.top = "-400%";
+    show = !show;
+  }
+})
+
+
+
+
+
