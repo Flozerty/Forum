90ae82524e806430cc58e7fee7df031ea1e9459b
Flozerty <floris.louerat@gmail.com>
2024-10-18 10:33:29 +0200

    fix indent

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Controller;
 
 use App\AbstractController;
@@ -8,16 +9,18 @@ use Model\Managers\CategoryManager;
 use Model\Managers\TopicManager;
 use Model\Managers\UserManager;
 
-class SecurityController extends AbstractController{
+class SecurityController extends AbstractController
+{
   // contiendra les méthodes liées à l'authentification : register, login et logout
 
-  
-        //////////////////////////////////////////////
-        ////////// TRAITEMENT D'UN REGISTER //////////
-        //////////////////////////////////////////////
-  public function register () {
 
-    if(isset($_POST["submit"])) {
+  //////////////////////////////////////////////
+  ////////// TRAITEMENT D'UN REGISTER //////////
+  //////////////////////////////////////////////
+  public function register()
+  {
+
+    if (isset($_POST["submit"])) {
 
       $userManager = new UserManager();
 
@@ -27,9 +30,10 @@ class SecurityController extends AbstractController{
       $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_VALIDATE_EMAIL);
 
       // fonction de redirection avec le message d'erreur
-      function redirectAndMessage($message) { 
+      function redirectAndMessage($message)
+      {
         return [
-          "view" => VIEW_DIR."register.php",
+          "view" => VIEW_DIR . "register.php",
           "meta_description" => "Page de création de compte",
           "data" => ["loginMessage" => $message]
         ];
@@ -45,46 +49,46 @@ class SecurityController extends AbstractController{
 
       // Si le mail est déja existant
       $userEmail = $userManager->findUserByEmail($email);
-      if($userEmail) {
+      if ($userEmail) {
         $message = "<p class='alertNotif'>Votre email est déjà attribué a un compte. Allez à la page de <a href='index.php?ctrl=security&action=login'>connexion</a>.</p>";
         return redirectAndMessage($message);
       }
 
       // si le pseudo existe déja
       $userPseudo = $userManager->findUserByPseudo($pseudo);
-      if($userPseudo) {
+      if ($userPseudo) {
         $message = "<p class='alertNotif'>Le pseudo existe déja. Veuillez en choisir un autre.</p>";
         return redirectAndMessage($message);
       }
-        
-      // si le mot de passe n'est pas conforme
-      if(strlen($pass1) <= 5) {
-        $message = "<p class='alertNotif'>Saisissez un mot de passe de 6 caractères minimum.</p>";
+
+      $passwordPattern = '/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/'; // 8 caractères, une majuscule, un chiffre
+      if (!preg_match($passwordPattern, $pass1)) {
+        $message = "<p class='alertNotif'>Le mot de passe doit contenir au moins 8 caractères, une majuscule et un chiffre.</p>";
         return redirectAndMessage($message);
       }
 
       // si les 2 mots de passe ne correspondent pas
-      if($pass1 != $pass2) {
+      if ($pass1 != $pass2) {
         $message = "<p class='alertNotif'>Les mots de passe ne correspondent pas</p>";
         return redirectAndMessage($message);
       }
 
       // Si tout est bon, on envoie
       $password = password_hash($pass1, PASSWORD_DEFAULT);
-      
+
       $message = "<p class='addNotif'>Votre compte a été créé, vous pouvez maintenant vous connecter.</p>";
-      
+
       $newUser = [
         "pseudo" => $pseudo,
         "email" => $email,
         "password" => $password,
       ];
-      
+
       $userManager->add($newUser);
-    Session::addFlash("success","compte créé avce succès");
+      Session::addFlash("success", "compte créé avce succès");
 
       return [
-        "view" => VIEW_DIR."login.php",
+        "view" => VIEW_DIR . "login.php",
         "meta_description" => "Page de connexion au forum",
         "data" => ["message" => $message]
       ];
@@ -92,17 +96,18 @@ class SecurityController extends AbstractController{
       // S'il n'y a aucun formulaire a traiter (on arrive seulement sur la page)
     } else {
       return [
-        "view" => VIEW_DIR."register.php",
+        "view" => VIEW_DIR . "register.php",
         "meta_description" => "Page de création de compte"
       ];
     }
   }
 
-        //////////////////////////////////////////////
-        /////////// TRAITEMENT D'UN LOGIN ////////////
-        //////////////////////////////////////////////
-  public function login () {
-    if(isset($_POST["submit"])) {
+  //////////////////////////////////////////////
+  /////////// TRAITEMENT D'UN LOGIN ////////////
+  //////////////////////////////////////////////
+  public function login()
+  {
+    if (isset($_POST["submit"])) {
 
       $userManager = new UserManager();
 
@@ -110,9 +115,10 @@ class SecurityController extends AbstractController{
       $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
       // fonction de redirection avec le message d'erreur
-      function redirectAndMessage($message) { 
+      function redirectAndMessage($message)
+      {
         return [
-          "view" => VIEW_DIR."login.php",
+          "view" => VIEW_DIR . "login.php",
           "meta_description" => "Page de création de compte",
           "data" => ["loginMessage" => $message]
         ];
@@ -123,7 +129,7 @@ class SecurityController extends AbstractController{
       // S'il manque un input
       if (!($pseudo && $password)) {
         $message = "<p class='alertNotif'>Merci de renseigner vos identifiants :)</p>";
-        Session::addFlash("error","trololol");
+        Session::addFlash("error", "trololol");
         return redirectAndMessage($message);
       }
 
@@ -131,65 +137,67 @@ class SecurityController extends AbstractController{
       $user = $userManager->findUserByPseudo($pseudo);
 
       // S'il ne trouve pas le pseudo
-      if(!$user) {
+      if (!$user) {
         $message = "<p class='alertNotif'>Pseudo ou mot de passe incorrect. Veuillez réessayer.</p>";
-        Session::addFlash("error","vérifiez vos identifiants");
+        Session::addFlash("error", "vérifiez vos identifiants");
         return redirectAndMessage($message);
       }
 
       $hach = $user->getPassword();
-      
+
       // Si le mot de passe est faux
-        if(!password_verify($password, $hach)) {
-        Session::addFlash("error","vérifiez vos identifiants");
+      if (!password_verify($password, $hach)) {
+        Session::addFlash("error", "vérifiez vos identifiants");
         $message = "<p class='alertNotif'>Pseudo ou mot de passe incorrect. Veuillez réessayer.</p>";
         return redirectAndMessage($message);
       }
-      
+
       // // Si tout est bon, on connecte l'user + redirect Home
       Session::setUser($user);
-    Session::addFlash("success","bonjour ".Session::getUser());
+      Session::addFlash("success", "bonjour " . Session::getUser());
 
       AbstractController::redirectTo($ctrl = "home", $action = "index", $id = null);
 
       // S'il n'y a aucun formulaire a traiter (on arrive seulement sur la page)
     } else {
       return [
-        "view" => VIEW_DIR."login.php",
+        "view" => VIEW_DIR . "login.php",
         "meta_description" => "Page de connexion",
       ];
     }
   }
-  
-        /////////////////// LOGOUT ///////////////////
-  public function logout () {
-    Session::addFlash("error","au revoir ".Session::getUser());
+
+  /////////////////// LOGOUT ///////////////////
+  public function logout()
+  {
+    Session::addFlash("error", "au revoir " . Session::getUser());
     Session::setUser(null);
     AbstractController::redirectTo($ctrl = "home", $action = "index", $id = null);
   }
-  
-        //////////////// CHANGE EMAIL ////////////////
-  public function changeEmail(){
+
+  //////////////// CHANGE EMAIL ////////////////
+  public function changeEmail()
+  {
     $newEmail = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
     $userManager = new UserManager();
 
     // on vérifie l'entrée de l'email
-    if(!$newEmail) {
-      Session::addFlash("error","veuillez saisir un email valide");
+    if (!$newEmail) {
+      Session::addFlash("error", "veuillez saisir un email valide");
       AbstractController::redirectTo($ctrl = "forum", $action = "myInfos");
     }
 
     $verifyEmail = $userManager->findUserByEmail($newEmail);
 
     // on vérifié si l'email existe déja
-    if($verifyEmail) {
-      Session::addFlash("error","cet email a déjà été attribué");
+    if ($verifyEmail) {
+      Session::addFlash("error", "cet email a déjà été attribué");
       AbstractController::redirectTo($ctrl = "forum", $action = "myInfos");
     }
-// on change l'email et on réattribue les données de l'user dans SESSION
+    // on change l'email et on réattribue les données de l'user dans SESSION
     $userManager->changeEmail($newEmail);
 
-    Session::addFlash("success","votre email a été modifié");
+    Session::addFlash("success", "votre email a été modifié");
 
     $userUpdated = $userManager->findOneById(Session::getUser()->getId());
     Session::setUser($userUpdated);
@@ -197,12 +205,13 @@ class SecurityController extends AbstractController{
     AbstractController::redirectTo($ctrl = "forum", $action = "myInfos");
   }
 
-        ////////////// CHANGE PASSWORD //////////////
-  public function changePassword() {
+  ////////////// CHANGE PASSWORD //////////////
+  public function changePassword()
+  {
     $actualPass = filter_input(INPUT_POST, "actualPass", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $newPass1 = filter_input(INPUT_POST, "newPass1", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $newPass2 = filter_input(INPUT_POST, "newPass2", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    
+
     $userManager = new UserManager();
 
     // on récupère l'user de la base de donnée. 
@@ -210,25 +219,25 @@ class SecurityController extends AbstractController{
     $user = $userManager->findOneById(Session::getUser()->getId());
     $hach = $user->getPassword();
 
-    if(!password_verify($actualPass, $hach)){
-      Session::addFlash("error"," vérifiez votre mot de passe");
+    if (!password_verify($actualPass, $hach)) {
+      Session::addFlash("error", " vérifiez votre mot de passe");
       AbstractController::redirectTo($ctrl = "forum", $action = "myInfos");
     }
 
-    if($actualPass == $newPass1) {
-      Session::addFlash("error","Choisissez un mot de passe différent");
+    if ($actualPass == $newPass1) {
+      Session::addFlash("error", "Choisissez un mot de passe différent");
       AbstractController::redirectTo($ctrl = "forum", $action = "myInfos");
     }
 
-    if($newPass1 != $newPass2) {
-      Session::addFlash("error"," les deux mots de passe ne sont pas identiques");
+    if ($newPass1 != $newPass2) {
+      Session::addFlash("error", " les deux mots de passe ne sont pas identiques");
       AbstractController::redirectTo($ctrl = "forum", $action = "myInfos");
     }
 
     $newPassword = password_hash($newPass1, PASSWORD_DEFAULT);
     $userManager->changePassword($newPassword);
-    Session::addFlash("success"," votre mot de passe a été mis à jour");
-    
+    Session::addFlash("success", " votre mot de passe a été mis à jour");
+
     $userUpdated = $userManager->findOneById(Session::getUser()->getId());
     Session::setUser($userUpdated);
 
@@ -236,21 +245,22 @@ class SecurityController extends AbstractController{
   }
 
   //////////////// CHANGE AVATAR ////////////////
-  public function changeAvatar(){
+  public function changeAvatar()
+  {
 
-    $target_dir = PUBLIC_DIR."img/avatar/";
+    $target_dir = PUBLIC_DIR . "img/avatar/";
     // le nom du fichier est $_FILES["avatar"]["name"]
     // on veut rename le fichier pour qu'il soit unique :
     // on utilise uniqid()
-    $original_fileName = $target_dir.basename($_FILES["avatar"]["name"]);
+    $original_fileName = $target_dir . basename($_FILES["avatar"]["name"]);
     $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($original_fileName,PATHINFO_EXTENSION));
-    $target_file = basename(uniqid().'.'.$imageFileType);
-    
-    if(isset($_POST["submit"])) {
+    $imageFileType = strtolower(pathinfo($original_fileName, PATHINFO_EXTENSION));
+    $target_file = basename(uniqid() . '.' . $imageFileType);
+
+    if (isset($_POST["submit"])) {
       $check = getimagesize($_FILES["avatar"]["tmp_name"]);
-      if($check !== false) {
-        echo"File is an image - " . $check["mime"] . ".";
+      if ($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
       } else {
         echo "File is not an image.";
@@ -259,31 +269,33 @@ class SecurityController extends AbstractController{
     }
 
     // si le fichier existe déja
-    if (file_exists($target_dir.$target_file)) {
-        Session::addFlash("error","Le fichier existe déjà, veuillez le renommer.");
+    if (file_exists($target_dir . $target_file)) {
+      Session::addFlash("error", "Le fichier existe déjà, veuillez le renommer.");
       $uploadOk = 0;
     }
-    
+
     // restriction de taille
     if ($_FILES["avatar"]["size"] > 500000) {
-        Session::addFlash("error","Fichier trop volumineux.");
+      Session::addFlash("error", "Fichier trop volumineux.");
       $uploadOk = 0;
     }
 
     // restriction de format
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
-        Session::addFlash("error","Seulement JPG, JPEG, PNG & GIF autorisés.");
+    if (
+      $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+      && $imageFileType != "gif"
+    ) {
+      Session::addFlash("error", "Seulement JPG, JPEG, PNG & GIF autorisés.");
       $uploadOk = 0;
     }
 
     if ($uploadOk == 0) {
-    // si tout est ok, on upload le fichier
+      // si tout est ok, on upload le fichier
     } else {
-      if (move_uploaded_file($_FILES["avatar"]["tmp_name"], $target_dir.$target_file)) {
-        Session::addFlash("success","Vous avez un nouvel avatar!");
+      if (move_uploaded_file($_FILES["avatar"]["tmp_name"], $target_dir . $target_file)) {
+        Session::addFlash("success", "Vous avez un nouvel avatar!");
       } else {
-        Session::addFlash("error","Désolé une erreur est survenue.");
+        Session::addFlash("error", "Désolé, une erreur est survenue.");
       }
     }
 
